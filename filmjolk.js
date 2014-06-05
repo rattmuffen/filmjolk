ead688fe4955819125b5f656c56951858c906b12
Anders <hagward@kth.se>
2014-06-05 17:02:18 +0900

    Default port, JSLint fixes
    + Filmjölk now uses a default port if none is specified, instead of
    listening to "port NaN".
    + Fixed some JSLint warnings.

--- filmjolk.js
+++ filmjolk.js
@@ -5,6 +5,9 @@ var express = require('express');
 var app = express();
 var async = require('async');
 
+// Default port.
+var port = 12345;
+
 try {
 	var fanart_api_key = fs.readFileSync('keys/fanarttv', 'utf8');
 	var tmdb_api_key = fs.readFileSync('keys/tmdb', 'utf8');
@@ -27,18 +30,24 @@ app.configure(function () {
 	app.use(express.methodOverride());
 });
 
-var port = parseInt(process.argv[2]);
+// Parse the port number from the command line argument, if specified.
+if (process.argv.length >= 3) {
+	var p = parseInt(process.argv[2], 10);
+	if (!isNaN(p)) {
+		port = p;
+	}
+}
 app.listen(port);
 console.log('Filmjolk is listening on port ' + port + '.');
 
 
-app.get('/movies/:channel/date/:date', function(req, res) {
+app.get('/movies/:channel/date/:date', function (req, res) {
 	// Waterfall flow: run functions in series, each passing their results to the next function.
 	async.waterfall([
 		// First, get movies for a channel and date from json.xmltv.se
 		function (callback) {
 			getMovies(req.params.channel, req.params.date, function (data) {
-				if (data == '') {
+				if (data === '') {
 					// If no movies on channel, just pass an empty array.
 					callback(null, []);
 				} else {
@@ -47,7 +56,7 @@ app.get('/movies/:channel/date/:date', function(req, res) {
 				}
 			});
 		},
-		function(movies, callback) {
+		function (movies, callback) {
 			if (movies) {
 				async.map(movies, search, function (err, results) {
 					callback(null, results);
@@ -55,7 +64,7 @@ app.get('/movies/:channel/date/:date', function(req, res) {
 			}
 		},
 		// Get TMDb info for each of the movies.
-		function(movies, callback){
+		function (movies, callback) {
 			if (movies) {
 				async.map(movies, getTMDB, function (err, results) {
 					callback(null, results);
@@ -63,31 +72,31 @@ app.get('/movies/:channel/date/:date', function(req, res) {
 			}
 		},
 		// Get TMDb trailers for each of the movies.
-		function(movies, callback){
+		function (movies, callback) {
 			async.map(movies, getTrailer, function (err, results) {
 				callback(null, results);
 			});
 		},
 		// Get fanart.tv data for each of the movies
-		function(movies, callback){
+		function (movies, callback) {
 			async.map(movies, getFanart, function (err, results) {
 				callback(null, results);
 			});
 		},
 		// Get IMDB data for each of the movies
-		function(movies, callback){
+		function (movies, callback) {
 			async.map(movies, getIMDB, function (err, results) {
 				callback(null, results);
 			});
 		},
 		// Get RT data for each of the movies
-		function(movies, callback){
+		function (movies, callback) {
 			async.map(movies, getRT, function (err, results) {
 				callback(null, results);
 			});
 		},
-		function(movies, callback){
-			async.map(movies,processMovie, function (err, results) {
+		function (movies, callback) {
+			async.map(movies, processMovie, function (err, results) {
 				callback(null, results);
 			});
 		}
@@ -102,11 +111,11 @@ function getMovies(id, date, callback) {
 	var options = {
 		host: 'api.epg.io',
 		port: 80,
-		path: '/2/movies/today/' + date +'/' + id + '?api_key=' + epgio_api_key,
+		path: '/2/movies/today/' + date + '/' + id + '?api_key=' + epgio_api_key,
 		method: 'GET'
 	};
 
-	var req = http.request(options, function(res) {
+	var req = http.request(options, function (res) {
 		var responseBody = "";
 		res.setEncoding('utf8');
 
@@ -114,10 +123,10 @@ function getMovies(id, date, callback) {
 			responseBody += chunk;
 		});
 
-		res.on('end',function() {
+		res.on('end', function () {
 			var result;
 			try {
-				result = JSON.parse(responseBody +'').movies;
+				result = JSON.parse(responseBody + '').movies;
 			} catch (err) {
 				req.end();
 				callback('');
@@ -127,12 +136,12 @@ function getMovies(id, date, callback) {
 		});
 	});
 	req.end();
-};
+}
 
 function search(item, callback) {
 	// TODO: check if tmdb-id or imdb-id already exists before searching.
 
-	tmdb.searchMovie({query: item.series.name}, function (err, result){
+	tmdb.searchMovie({query: item.series.name}, function (err, result) {
 		if (result && result.results.length > 0) {
 			item.url = 'http://www.themoviedb.org/movie/' + result.results[0].id;
 			item.id = result.results[0].id;
@@ -145,14 +154,14 @@ function search(item, callback) {
 }
 
 function getTMDB(item, callback) {
-	tmdb.movieInfo({id: item.id}, function (err, result){
+	tmdb.movieInfo({id: item.id}, function (err, result) {
 		item.tmdb = result;
 		callback(null, item);
 	});
 }
 
 function getTrailer(item, callback) {
-	tmdb.movieTrailers({id: item.id}, function (err, result){
+	tmdb.movieTrailers({id: item.id}, function (err, result) {
 		item.trailer = result;
 		callback(null, item);
 	});
@@ -160,7 +169,7 @@ function getTrailer(item, callback) {
 
 function getIMDB(item, callback) {
 	if (item.tmdb) {
-		imdb.getReq({ id: item.tmdb.imdb_id }, function(err, info) {
+		imdb.getReq({ id: item.tmdb.imdb_id }, function (err, info) {
 			item.imdb = info;
 			callback(null, item);
 		});
@@ -178,23 +187,23 @@ function getFanart(item, callback) {
 		method: 'GET'
 	};
 
-	var request = http.request(options, function(resp) {
+	var request = http.request(options, function (resp) {
 		var responseBody = "";
 		resp.setEncoding('utf8');
 
-		resp.on('data', function(chunk) {responseBody += chunk; });
+		resp.on('data', function (chunk) {responseBody += chunk; });
 
-		resp.on('end', function() {
+		resp.on('end', function () {
 			request.end();
 			item.fanart = parseFanartResult(responseBody);
 			callback(null, item);
 		});
 
-		resp.on('close',function() {
+		resp.on('close', function () {
 			request.end();
 		});
 
-		resp.on('error',function() {
+		resp.on('error', function () {
 			request.end();
 		});
 	});
@@ -202,11 +211,12 @@ function getFanart(item, callback) {
 }
 
 function getRT(item, callback) {
-	tomatoes.search(item.series.name, function (err, result){
-		if (result.length > 0)
+	tomatoes.search(item.series.name, function (err, result) {
+		if (result.length > 0) {
 			item.rt = result[0];
-		else
+		} else {
 			item.rt = '';
+		}
 
 		callback(null, item);
 	});
@@ -214,9 +224,9 @@ function getRT(item, callback) {
 
 // Removes the top JSON-object and shift children to top.
 function parseFanartResult(data) {
-	var data = data.split("{").slice(2).join('{').slice(0, -1);
+	data = data.split('{').slice(2).join('{').slice(0, -1);
 
-	if (data == '') {return data; }
+	if (data === '') { return data; }
 	return JSON.parse('{' + data);
 }
 
@@ -225,7 +235,7 @@ function processMovie(item, callback) {
 	var backgrounds = [];
 
 	if (item.tmdb && item.tmdb.backdrop_path) {
-		backgrounds.push({'url':'http://image.tmdb.org/t/p/original' + item.tmdb.backdrop_path})
+		backgrounds.push({'url': 'http://image.tmdb.org/t/p/original' + item.tmdb.backdrop_path});
 	}
 
 	if (item.fanart.moviebackground) {
@@ -238,22 +248,22 @@ function processMovie(item, callback) {
 	var score = 0;
 
 	if (item.imdb && item.imdb.rating) {
-		sources++;
+		sources += 1;
 		score += parseFloat(item.imdb.rating) * 10;
 	}
 
 	if (item.tmdb && item.tmdb.vote_average) {
-		sources++;
-		score += item.tmdb.vote_average * 10
+		sources += 1;
+		score += item.tmdb.vote_average * 10;
 	}
 
-	if (item.rt && item.rt.ratings.audience_score && item.rt.ratings.audience_score != -1) {
-		sources++;
+	if (item.rt && item.rt.ratings.audience_score && item.rt.ratings.audience_score !== -1) {
+		sources += 1;
 		score += item.rt.ratings.audience_score;
 	}
 
-	if (item.rt && item.rt.ratings.critics_score && item.rt.ratings.critics_score != -1) {
-		sources++;
+	if (item.rt && item.rt.ratings.critics_score && item.rt.ratings.critics_score !== -1) {
+		sources += 1;
 		score += item.rt.ratings.critics_score;
 	}
 
@@ -272,8 +282,9 @@ function processMovie(item, callback) {
 
 
 	item.backgrounds = backgrounds;
-	if (sources > 0)
+	if (sources > 0) {
 		item.avg_score = Math.round(score/sources);
+	}
 
 	callback(null, item)
 }
